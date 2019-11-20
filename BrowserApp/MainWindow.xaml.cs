0974584c865e4b0de523b96621c7727f045d316a
esim7 <makishev@yahoo.com>
2019-11-20 20:30:17 +0600

    Сделал за 1 коммит :) Больше коммитов нет

---
+++ BrowserApp/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BrowserApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public WebBrowser browser { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Back_click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.browser.GoBack();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void Forward_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                this.browser.GoForward();
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message);
+            }
+        }
+
+        private void Go_Click(object sender, RoutedEventArgs e)
+        {
+            if (tabControl.Items.Count == 0)
+            {
+                AddPage_Click(sender, e);
+            }
+            else
+            {
+                if (textBox_URL.Text.Length > 0)
+                {
+                    this.browser.Source = new Uri("https://" + textBox_URL.Text);
+                }
+                else
+                {
+                    MessageBox.Show("Invalid URL");
+                }
+            }
+
+        }
+
+        private void AddPage_Click(object sender, RoutedEventArgs e)
+        {
+            browser = new WebBrowser();
+            TabItem tabItem = new TabItem() 
+            { 
+                Header = "NewPage", 
+                Content = browser as WebBrowser
+            };
+            tabControl.Items.Add(tabItem);
+        }
+    }
+}
